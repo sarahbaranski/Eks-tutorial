54922c77decf0efbd2fec178de55ac36347f4072
sarahbaranski <sbaranski@gmail.com>
2025-08-08 08:47:20 -0500

    feat: add eks cluster

---
+++ eks.tf
@@ -0,0 +1,49 @@
+data "aws_iam_policy_document" "eks_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+        "sts:AssumeRole"
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }   
+  }
+}
+
+resource "aws_iam_role" "eks" {
+    name = "${local.env}-${local.eks_name}-eks-cluster"
+
+    assume_role_policy = data.aws_iam_policy_document.eks_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.eks.name 
+}
+
+# For the eks cluster itself
+resource "aws_eks_cluster" "eks" {
+  name = "${local.env}-${local.eks_name}"
+  version = local.eks_version
+  role_arn = aws_iam_role.eks.arn
+
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access = true
+
+# where worker nodes will be deployed
+    subnet_ids = [
+      aws_subnet.private_zone1.id,
+      aws_subnet.private_zone2.id
+    ]
+  }
+
+  access_config {
+    authentication_mode = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  depends_on = [ aws_iam_role_policy_attachment.eks]
+}
